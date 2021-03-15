6f620f6156b8d69f22a4fb7f7f13289028896bfc
JP Baez <baezjuanpablo@gmail.com>
2021-03-15 17:52:38 +0100

    update

--- src/script.js
+++ src/script.js
@@ -1,22 +1,46 @@
 
 const newInput = document.querySelector('#newEntry');
 const form = document.querySelector(".taskAdder");
-let listEl = document.getElementById('myTasks');
+const listEl = document.getElementById('myTasks');
+const clearEl = document.querySelector('#clearAll') 
  
 
+        //add items
 
 form.addEventListener('submit', (event)=>{
     event.preventDefault(); 
     addNewTodo(event);
-    console.log(newInput) 
     newInput.value = '';
 })
-    
-
 function addNewTodo(event)  {
-let template = `<li class="todo-item">${newInput.value}</li>`; 
-listEl.innerHTML += template;
+    let template = `<li class="todo-item"><p contenteditable>${newInput.value}</p> 
+    <div class="itemButtons"> <img class="editIcon" src="src/images/iconedit.svg"> 
+    <img class="closeIcon" src="src/images/iconDelete.svg"></div></li>`; 
+    listEl.innerHTML += template;
+}
+       
+
+listEl.addEventListener('click', (event) =>{
+    const clickedEl = event.target;
+    if (clickedEl.classList.contains("closeIcon")) {       
+    eraseTodo(clickedEl);}
+    if (clickedEl.classList.contains("editIcon")) {
+    editTask(clickedEl);}
+    console.log(event.target)
+})
+
+     // Edit Items
+const editTask = () => {
+    
 }
 
 
+    //delete Items
 
+const eraseTodo = (targetItem) => {
+    listEl.removeChild(targetItem.closest('li'));
+}
+
+clearEl.addEventListener('click', () =>{
+    listEl.innerHTML = '';
+})
