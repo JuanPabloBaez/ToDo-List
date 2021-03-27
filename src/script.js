134125c27c8087af853992a78b1f369cbd3e021e
JP Baez <baezjuanpablo@gmail.com>
2021-03-27 12:35:53 +0100

    update

--- src/script.js
+++ src/script.js
@@ -2,47 +2,107 @@
 const newInput = document.querySelector('#newEntry');
 const form = document.querySelector(".taskAdder");
 const listEl = document.getElementById('myTasks');
-const clearEl = document.querySelector('#clearAll') 
- 
+const clearEl = document.querySelector('#clearAll'); 
+let todos = JSON.parse(localStorage.getItem('items')) || [];
+render();
+
 
         //add items
-
 form.addEventListener('submit', (event)=>{
     event.preventDefault(); 
     addNewTodo(event);
     newInput.value = '';
 })
-function addNewTodo(event)  {
-    let template = `<li class="todo-item"><input type="checkbox" id="horns"> <p class="inputText" contenteditable>${newInput.value}</p> 
-    <div class="itemButtons"> <img class="editIcon" src="src/images/iconedit.svg"> 
-    <img class="closeIcon" src="src/images/iconDelete.svg"></div></li>`; 
-    listEl.innerHTML += template;
+const addNewTodo = () => {
+  if (newInput.value === "") alert("cannot be empty")
+  const newTodo = {
+    id: (Date.now() + Math.random()).toString(),
+    title: newInput.value,
+    //isEditable: true,
+    isDone: false
+  }
+  todos.push(newTodo);
+  save()
+  render();
+  
 }
-       
+function render() {
+  clearElements();
+  todos.forEach ((todo) => {
+    const template = `
+    <li data-id = ${todo.id}>
+        <input type='checkbox' ${todo.isDone ? "checked" : null} />
+        <p class=' ${todo.isDone ?  "todo-item completed" : "todo-item"}' id='itemText' contenteditable='${!todo.isDone}'>${todo.title}
+      </p>
+      <img class="closeIcon" src="src/images/iconDelete.svg"/>
+    </li>`;
+    listEl.insertAdjacentHTML("beforeend", template);
+  })
+}
+function clearElements() {
+  listEl.innerHTML = "";}
+  
 
+
+    
 listEl.addEventListener('click', (event) =>{
-    const clickedEl = event.target;
-    if (clickedEl.classList.contains("closeIcon")) {       
-    eraseTodo(clickedEl);}
-    if (clickedEl.classList.contains("editIcon")) {
-    editTask(clickedEl);}
-    console.log(event.target)
-})
+     let clickedEl = event.target;
+      if (clickedEl.classList.contains("closeIcon")) {       
+        eraseTodo(clickedEl);
+      }
+                     // Edit Items
+      if (clickedEl.tagName.toLowerCase() === "p") {
+        clickedEl.onkeydown = (event) => {
+          if (event.key === "Enter") {
+            event.preventDefault(); // prevents line breaks
+            const newText = clickedEl.textContent;
+            if (clickedEl.textContent.length < 2) {
+              return alert("Todo item cannot be empty or less then two chars.");
+            }
+            const clickedItemId = clickedEl.parentElement.dataset.id;   // find data-id from clicked html item
+            const currTodo = todos.find((todo) => clickedItemId === todo.id);   // find current todo obj based on id
+            currTodo.title = newText;   // update clicked todo title with edited text 
+            currTodo.isEditable = false;     // update isEditable property for our curent todo Item
+            save()
+            render();
+          }
+        };
+      }
+          //completed items 
+      if (clickedEl.tagName.toLowerCase() === 'input') {
+        //let checkEl = clickedEl;
+        let checkId = clickedEl.parentElement.dataset.id;
+        const checkedItem = todos.find((todo) => checkId === todo.id);
+        console.log(checkedItem)
+        
+        if (clickedEl.checked) {
+          checkedItem.isDone = true;
+          save();
+          render();
+        } else {
+          checkedItem.isDone = false;
+          save();
+          render();
+        }    
+      }
+    })
 
-     // Edit Items
-
-const editTask = (event) => {
-const editText = document.getElementById("inputText")
-
+          //delete Items
+const eraseTodo = (clickedEl) => {
+  const clickedItemId = clickedEl.parentElement.dataset.id;
+  todos = todos.filter((todo, index) => {
+  return todo.id !== clickedItemId;
+  })
+  save();
+  render(); 
 }
-
-
-    //delete Items
-
-const eraseTodo = (targetItem) => {
-    listEl.removeChild(targetItem.closest('li'));
-}
-
-clearEl.addEventListener('click', () =>{
-    listEl.innerHTML = '';
+clearEl.addEventListener('click', () =>{   
+    //listEl.innerHTML = '';
+    todos.length = 0;
+    save();
+   render();   
 })
+  
+function save() {
+  localStorage.setItem('items', JSON.stringify(todos)); 
+}
